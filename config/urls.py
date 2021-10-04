bad39915445d5faab687929c84c7e771142f14b5
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2021-10-04 18:33:18 +0300

    Adds home page redirect

--- config/urls.py
+++ config/urls.py
@@ -3,21 +3,21 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
-    # Django Admin, use {% url 'admin:index' %}
-    path(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    path("users/", include("happy_dogs.users.urls", namespace="users")),
-    path("accounts/", include("allauth.urls")),
-    path("dogs/", include("happy_dogs.dogs.urls", namespace="dogs"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+                  path("", RedirectView.as_view(pattern_name='dogs:boarding'), name="home"),
+                  path(
+                      "about/", TemplateView.as_view(template_name="pages/about.html"),
+                      name="about"
+                  ),
+                  # Django Admin, use {% url 'admin:index' %}
+                  path(settings.ADMIN_URL, admin.site.urls),
+                  # User management
+                  path("users/", include("happy_dogs.users.urls", namespace="users")),
+                  path("accounts/", include("allauth.urls")),
+                  path("dogs/", include("happy_dogs.dogs.urls", namespace="dogs"))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
