adb5d1dc59fc3ef60a7dd1ef6528497592587159
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2021-10-04 11:29:50 +0300

    Creates Dogs, Visit models

--- happy_dogs/dogs/models.py
+++ happy_dogs/dogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+# TODO: Add admin
+# TODO: Add fields attributes
+class Dog(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Visit(models.Model):
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
