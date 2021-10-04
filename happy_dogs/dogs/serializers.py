fed0a285b934ac018a197b5a03cd46c2e9f9c30c
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2021-10-05 02:29:46 +0300

    Adds React

--- happy_dogs/dogs/serializers.py
+++ happy_dogs/dogs/serializers.py
@@ -1,9 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from happy_dogs.dogs.models import BoardingVisit
 
 
-class BoardingVisitSerializer(ModelSerializer):
+class BoardingVisitSerializer(serializers.ModelSerializer):
+    dog = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='full_name'
+    )
+
     class Meta:
         model = BoardingVisit
         fields = ['dog', 'start_date', 'end_date']
--- happy_dogs/static/js/project.js
+++ happy_dogs/static/js/project.js
@@ -1,1 +1,79 @@
-/* Project specific Javascript goes here. */
+const host = window.location.protocol + "//" + window.location.host;
+
+const useFetchAPI = (url) => {
+  const [error, setError] = React.useState(null);
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [items, setItems] = React.useState([]);
+
+  console.log(url)
+
+  React.useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [url])
+  return [error, isLoaded, items];
+}
+
+const Boarding = ({setBoardingDayUrl}) => {
+  const [error, isLoaded, items] = useFetchAPI('/dogs/boarding-api/')
+
+  let boarding_records = [...items];
+  boarding_records.forEach((boarding_record) => {
+    boarding_record.url = `${host}/dogs/boarding-api/${boarding_record.date.replaceAll('-', '/')}/`
+  })
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (boarding_records.map((boarding_record, index) => (
+      <div key={index} onClick={(e) => setBoardingDayUrl(boarding_record.url)}>
+        {boarding_record.date} - {boarding_record.dogs_count}
+      </div>
+    )))
+  }
+}
+
+const BoardingDay = ({boardingDayUrl}) => {
+  const [error, isLoaded, items] = useFetchAPI(boardingDayUrl)
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (items.map((item, index) => (
+      <div key={index}>
+        {item.dog}: {item.start_date} - {item.end_date}
+      </div>
+    )))
+  }
+}
+
+const App = () => {
+  const [boardingDayUrl, setBoardingDayUrl] = React.useState('/dogs/boarding-api/2021/10/5/')
+  return (
+    <div>
+      <Boarding setBoardingDayUrl={setBoardingDayUrl}/>
+      <BoardingDay boardingDayUrl={boardingDayUrl}/>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
